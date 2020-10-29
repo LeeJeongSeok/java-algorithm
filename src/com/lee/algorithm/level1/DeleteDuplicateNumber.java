8fee978f0db00c7a0221c8a3d6c98582de6a5e2e
LeeJeongSeok <ljs0429777@gmail.com>
2020-10-30 01:05:44 +0900

    fix : 로직 수정 중..

--- src/com/lee/algorithm/level1/DeleteDuplicateNumber.java
+++ src/com/lee/algorithm/level1/DeleteDuplicateNumber.java
@@ -2,13 +2,16 @@ package com.lee.algorithm.level1;
 
 public class DeleteDuplicateNumber {
     public static void main(String[] args) {
+        //int[] arr = {1, 1, 3, 3, 0, 1, 1};
         int[] arr = {4, 4, 4, 3, 3};
         int count = arr.length;
 
-        for (int i = 0; i < count; i++) {
+        for (int i = 0; i < count-1; i++) {
             if (arr[i] == arr[i+1]) {
                 arr[i] = arr[i+1];
-                count--;
+                if (i == arr.length) {
+
+                }
                 continue;
             }
             System.out.println(arr[i]);
