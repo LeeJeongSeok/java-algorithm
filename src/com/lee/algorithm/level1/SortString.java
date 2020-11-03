03f449d6b5861e8fa4cfe8d9c9a877a530c1e4b1
LeeJeongSeok <ljs0429777@gmail.com>
2020-11-03 22:45:12 +0900

    update : 문자열 정렬 문제 추가

---
+++ src/com/lee/algorithm/level1/SortString.java
@@ -0,0 +1,15 @@
+package com.lee.algorithm.level1;
+
+public class SortString {
+
+    public String[] solution(String[] strings, int n) {
+
+        String[] answer = strings;
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new SortString().solution(new String[]{"sun", "bed", "car"}, 1));
+    }
+}
