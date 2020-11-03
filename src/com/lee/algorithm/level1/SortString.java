3338c6c73c3edc81c76cf5b7d128a1dfc170fdd4
LeeJeongSeok <ljs0429777@gmail.com>
2020-11-03 23:55:56 +0900

    fixing.. : 문자열 내 마음대로 정렬하기 푸는 중..

--- src/com/lee/algorithm/level1/SortString.java
+++ src/com/lee/algorithm/level1/SortString.java
@@ -1,15 +1,30 @@
 package com.lee.algorithm.level1;
 
+import java.util.Arrays;
+
 public class SortString {
 
     public String[] solution(String[] strings, int n) {
 
-        String[] answer = strings;
+        String[] answer = {};
 
+        char[] a = new char[strings.length];
+
+        for (int i = 0; i < strings.length; i++) {
+            String index = strings[i];
+            a[i] = index.charAt(n);
+        }
+
+        Arrays.sort(a);
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+
+        }
         return answer;
     }
 
     public static void main(String[] args) {
-        System.out.println(new SortString().solution(new String[]{"sun", "bed", "car"}, 1));
+        new SortString().solution(new String[]{"sun", "bed", "car"}, 1);
     }
 }
