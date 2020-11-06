47f6fea95549bc5bdb2de303a1766e86b782e3d6
LeeJeongSeok <ljs0429777@gmail.com>
2020-11-07 01:33:20 +0900

    update : 소수 찾기 풀이

---
+++ src/com/lee/algorithm/level1/FindPrimeNumbers.java
@@ -0,0 +1,28 @@
+package com.lee.algorithm.level1;
+
+public class FindPrimeNumbers {
+    public int solution(int n) {
+
+        int answer = 0;
+
+        for (int i = 2; i < n; i++) {
+            boolean flag = true;
+           for (int j = 2; j<= i; j++) {
+               if (i % j == 0) {
+                   flag = false;
+                   break;
+               }
+           }
+
+           if (flag = true) {
+               answer++;
+           }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        new FindPrimeNumbers().solution(10);
+    }
+}
