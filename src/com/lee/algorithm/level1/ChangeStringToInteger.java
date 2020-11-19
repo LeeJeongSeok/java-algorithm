f260ce0faa7b4fd1f85d566812621b30e0029934
LeeJeongSeok <ljs0429777@gmail.com>
2020-11-19 22:51:37 +0900

    update : 문자열을 정수로 바꾸기 문제 풀이

--- src/com/lee/algorithm/level1/ChangeStringToInteger.java
+++ src/com/lee/algorithm/level1/ChangeStringToInteger.java
@@ -1,4 +1,14 @@
 package com.lee.algorithm.level1;
 
 public class ChangeStringToInteger {
+    public int solution(String s) {
+
+        int answer = Integer.parseInt(s);
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        new ChangeStringToInteger().solution("+1234");
+    }
 }
