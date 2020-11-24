bae7a47ddf04f8f501ca9eb82fc44769f326927b
LeeJeongSeok <ljs0429777@gmail.com>
2020-11-24 22:43:07 +0900

    update : 소수 찾기 문제 풀이

---
+++ src/com/lee/algorithm/level1/CreateStrangeCharacters.java
@@ -0,0 +1,28 @@
+package com.lee.algorithm.level1;
+
+public class CreateStrangeCharacters {
+    public String solution(String s) {
+
+
+        String answer = "try";
+
+        int count = answer.length();
+
+        for (int i = 0; i < count; i++) {
+
+        }
+
+
+
+
+        System.out.println(answer);
+
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        new CreateStrangeCharacters().solution("try hello world");
+    }
+}
--- src/com/lee/algorithm/level1/FindPrimeNumbers.java
+++ src/com/lee/algorithm/level1/FindPrimeNumbers.java
@@ -5,19 +5,14 @@ public class FindPrimeNumbers {
 
         int answer = 0;
 
-        for (int i = 2; i < n; i++) {
-            boolean flag = true;
-           for (int j = 2; j<= i; j++) {
-               if (i % j == 0) {
-                   flag = false;
-                   break;
-               }
-           }
-
-           if (flag == true) {
-               answer++;
-           }
+        for (int i = 2; i <= n; i++) {
+            if (n % i == 0) {
+                return 0;
+            }
+            answer++;
         }
+
+        System.out.println(answer);
 
         return answer;
     }
