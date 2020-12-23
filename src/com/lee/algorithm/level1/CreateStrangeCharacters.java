25c57f0a58620ab61268582f964a9701e3bc9518
LeeJeongSeok <ljs0429777@gmail.com>
2020-12-24 01:46:12 +0900

    프로그래머스 : 이상한 문자 만들기 풀이 (성능이슈로 인해 수정해야함)

--- src/com/lee/algorithm/level1/CreateStrangeCharacters.java
+++ src/com/lee/algorithm/level1/CreateStrangeCharacters.java
@@ -1,25 +1,57 @@
 package com.lee.algorithm.level1;
 
+/*
+문제 설명
+문자열 s는 한 개 이상의 단어로 구성되어 있습니다. 각 단어는 하나 이상의 공백문자로 구분되어 있습니다. 각 단어의 짝수번째 알파벳은 대문자로, 홀수번째 알파벳은 소문자로 바꾼 문자열을 리턴하는 함수, solution을 완성하세요.
+
+제한 사항
+문자열 전체의 짝/홀수 인덱스가 아니라, 단어(공백을 기준)별로 짝/홀수 인덱스를 판단해야합니다.
+첫 번째 글자는 0번째 인덱스로 보아 짝수번째 알파벳으로 처리해야 합니다.
+입출력 예
+s	            return
+try hello world	TrY HeLlO WoRlD
+입출력 예 설명
+try hello world는 세 단어 try, hello, world로 구성되어 있습니다. 각 단어의 짝수번째 문자를 대문자로, 홀수번째 문자를 소문자로 바꾸면 TrY, HeLlO, WoRlD입니다. 따라서 TrY HeLlO WoRlD 를 리턴합니다.
+ */
+
 public class CreateStrangeCharacters {
     public String solution(String s) {
 
+        String answer = "";
 
-        String answer = "try";
+        String[] strings = s.split(" ");
 
-        int count = answer.length();
+        for (int i = 0; i < strings.length; i++) {
+            String words = strings[i];
 
-        for (int i = 0; i < count; i++) {
+            char[] word = words.toCharArray();
+
+            for (int j = 0; j < word.length; j++) {
+                if (j % 2 == 0) {
+                    word[j] = changeUpper(word[j]);
+                }
+            }
+
+            if (i == strings.length - 1) {
+                answer += String.valueOf(word);
+            } else {
+                answer += String.valueOf(word) + " ";
+            }
 
         }
 
-
-
-
-        System.out.println(answer);
-
-
-
         return answer;
+
+    }
+
+
+    private char changeUpper(char word) {
+        if (word > 'a' && word < 'z') {
+            word = (char) (word - 32);
+        } else {
+            word = (char) (word + 32);
+        }
+        return word;
     }
 
     public static void main(String[] args) {
