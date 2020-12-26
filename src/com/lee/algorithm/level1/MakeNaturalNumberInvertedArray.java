cd48bedcbba6b18967fa24628158c8f787d42e5a
LeeJeongSeok <ljs0429777@gmail.com>
2020-12-26 15:58:59 +0900

    프로그래머스 자연수 뒤집어 배열로 만들기 푸는 중..

---
+++ src/com/lee/algorithm/level1/MakeNaturalNumberInvertedArray.java
@@ -0,0 +1,36 @@
+package com.lee.algorithm.level1;
+
+/*
+문제 설명
+자연수 n을 뒤집어 각 자리 숫자를 원소로 가지는 배열 형태로 리턴해주세요. 예를들어 n이 12345이면 [5,4,3,2,1]을 리턴합니다.
+
+제한 조건
+n은 10,000,000,000이하인 자연수입니다.
+
+입출력 예
+n	    return
+12345	[5,4,3,2,1]
+ */
+
+import java.lang.reflect.Array;
+
+public class MakeNaturalNumberInvertedArray {
+    public int[] solution(long n) {
+
+        int[] answer = {};
+
+        int i = 0;
+
+        while (n % 10 != 0) {
+            answer[i] = (int)n % 10;
+            n = n / 10;
+            i++;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        new MakeNaturalNumberInvertedArray().solution(12345);
+    }
+}
