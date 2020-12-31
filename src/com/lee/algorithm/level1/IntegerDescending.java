303a3600b15cdac631ea665474672934fd567ac2
LeeJeongSeok <ljs0429777@gmail.com>
2020-12-31 22:59:52 +0900

    프로그래머스 level1 정수 내림차순으로 배치하기 코드 수정

--- src/com/lee/algorithm/level1/IntegerDescending.java
+++ src/com/lee/algorithm/level1/IntegerDescending.java
@@ -6,20 +6,25 @@ import java.util.Collections;
 public class IntegerDescending {
     public long solution(long n) {
 
-        String[] numbers = Integer.toString((int)n).split("");
-        String number = "";
+        long answer = 0;
+
+        String[] numbers = String.valueOf(n).split("");
         Arrays.sort(numbers, Collections.reverseOrder());
+
+        String number = "";
 
         for (int i = 0; i < numbers.length; i++) {
             number += numbers[i];
         }
 
-        long answer = Long.parseLong(number);
+        answer = Long.parseLong(number);
+
+        System.out.println(answer);
 
         return answer;
     }
 
     public static void main(String[] args) {
-        new IntegerDescending().solution(118372);
+        new IntegerDescending().solution(809120300);
     }
 }
