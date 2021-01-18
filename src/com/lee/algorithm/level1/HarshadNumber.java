c06a12497d1e50d328a78a899dccfa494c69be70
LeeJeongSeok <ljs0429777@gmail.com>
2021-01-18 20:01:29 +0900

    add : 프로그래머스 level1 하샤드 수 풀이 추가

--- src/com/lee/algorithm/level1/HarshadNumber.java
+++ src/com/lee/algorithm/level1/HarshadNumber.java
@@ -5,11 +5,23 @@ public class HarshadNumber {
 
         boolean answer = true;
 
+        int temp = x;
+        int sum = 0;
+
+        while (temp != 0) { // 1의 자리부터 거꾸로 나머지를 계산한 후 몫을 구하는 형식으로 풀어야 한다.
+            sum += temp % 10;
+            temp = temp / 10;
+        }
+
+        if (x % sum != 0) {
+            answer = false;
+        };
+
         return answer;
 
     }
 
     public static void main(String[] args) {
-        new HarshadNumber().solution(12);
+        new HarshadNumber().solution(13);
     }
 }
