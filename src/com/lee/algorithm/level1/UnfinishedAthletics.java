aae77732cb33d3b7a7a6533a1a3ae82e12451d7d
LeeJeongSeok <ljs0429777@gmail.com>
2021-01-19 23:41:24 +0900

    add : 프로그래머스 level1 완주하지 못한 선수 풀이 중..

---
+++ src/com/lee/algorithm/level1/UnfinishedAthletics.java
@@ -0,0 +1,12 @@
+package com.lee.algorithm.level1;
+
+public class UnfinishedAthletics {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        new UnfinishedAthletics().solution(new String[]{"lee", "kiki", "eden"}, new String[]{"eden", "kiki"});
+    }
+}
