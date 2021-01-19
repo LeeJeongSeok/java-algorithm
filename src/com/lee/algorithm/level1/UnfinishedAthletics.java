4cf20b9ffa6e63c1214b38a7b5041f6383e3a807
LeeJeongSeok <ljs0429777@gmail.com>
2021-01-20 00:37:53 +0900

    fix : 프로그래머스 level1 완주하지 못한 선수 효율성 다시 고려해보기..

--- src/com/lee/algorithm/level1/UnfinishedAthletics.java
+++ src/com/lee/algorithm/level1/UnfinishedAthletics.java
@@ -1,12 +1,29 @@
 package com.lee.algorithm.level1;
 
+import java.util.Arrays;
+
+
 public class UnfinishedAthletics {
     public String solution(String[] participant, String[] completion) {
+
         String answer = "";
+
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+
+        for (int i = 0; i < completion.length; i++) {
+            for (int j = 0; j < participant.length; j++) {
+                if (!participant[j].equals(completion[i])) {
+                    answer = participant[j];
+                }
+            }
+        }
+
+        System.out.println(answer);
         return answer;
     }
 
     public static void main(String[] args) {
-        new UnfinishedAthletics().solution(new String[]{"lee", "kiki", "eden"}, new String[]{"eden", "kiki"});
+        new UnfinishedAthletics().solution(new String[]{"mislav", "stanko", "mislav", "ana"}, new String[]{"stanko", "ana", "mislav"});
     }
 }
