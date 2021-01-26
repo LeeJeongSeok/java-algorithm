b3245c75bb14104c827aa3778ef6f3eb37d9364c
LeeJeongSeok <ljs0429777@gmail.com>
2021-01-26 23:13:45 +0900

    add : 프로그래머스 level1

---
+++ src/com/lee/algorithm/level1/MatrixAddition.java
@@ -0,0 +1,12 @@
+package com.lee.algorithm.level1;
+
+public class MatrixAddition {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = {};
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        new MatrixAddition().solution(new int[][]{{1,2}, {2,3}}, new int[][]{{3, 4},{5, 6}});
+    }
+}
