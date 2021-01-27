435d3ffb5ba4cb7c8e62a3b61c0d88bb9642710b
LeeJeongSeok <ljs0429777@gmail.com>
2021-01-27 17:14:51 +0900

    add : 프로그래머스 level1 핼렬의 덧셈 풀이 추가

--- src/com/lee/algorithm/level1/MatrixAddition.java
+++ src/com/lee/algorithm/level1/MatrixAddition.java
@@ -2,11 +2,18 @@ package com.lee.algorithm.level1;
 
 public class MatrixAddition {
     public int[][] solution(int[][] arr1, int[][] arr2) {
-        int[][] answer = {};
+        int[][] answer = new int[arr1.length][arr2[0].length];
+
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr2[0].length; j++) {
+                answer[i][j] = arr1[i][j] + arr2[i][j];
+            }
+        }
         return answer;
     }
 
     public static void main(String[] args) {
-        new MatrixAddition().solution(new int[][]{{1,2}, {2,3}}, new int[][]{{3, 4},{5, 6}});
+//        new MatrixAddition().solution(new int[][]{{1,2}, {2,3}}, new int[][]{{3, 4},{5, 6}});
+        new MatrixAddition().solution(new int[][]{{1},{2}}, new int[][]{{3},{4}});
     }
 }
