1c186aeca8af898489c3604b704ec951f41cf315
LeeJeongSeok <ljs0429777@gmail.com>
2021-01-27 17:15:28 +0900

    add : 프로그래머스 level1 행렬의 덧셈 풀이 주석 추

--- src/com/lee/algorithm/level1/MatrixAddition.java
+++ src/com/lee/algorithm/level1/MatrixAddition.java
@@ -2,7 +2,7 @@ package com.lee.algorithm.level1;
 
 public class MatrixAddition {
     public int[][] solution(int[][] arr1, int[][] arr2) {
-        int[][] answer = new int[arr1.length][arr2[0].length];
+        int[][] answer = new int[arr1.length][arr2[0].length]; // answer의 크키는 x로는 arr1만큼, y로는 arr2 만큼 추가
 
         for (int i = 0; i < arr1.length; i++) {
             for (int j = 0; j < arr2[0].length; j++) {
