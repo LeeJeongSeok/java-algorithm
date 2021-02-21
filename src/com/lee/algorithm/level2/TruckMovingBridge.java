ffe8d965041c607cf4411c9dd4184e225bb3d26d
LeeJeongSeok <ljs0429777@gmail.com>
2021-02-21 16:44:14 +0900

    프로그래머스 level2 다리를 지나는 트럭 풀이 중..

---
+++ src/com/lee/algorithm/level2/TruckMovingBridge.java
@@ -0,0 +1,24 @@
+package com.lee.algorithm.level2;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class TruckMovingBridge {
+    public static void main(String[] args) {
+        // 7,4,5,6
+
+
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(7);
+
+
+        for (int i = 0; i < 2; i++) {
+            String arg = args[i];
+
+        }
+
+
+    }
+}
